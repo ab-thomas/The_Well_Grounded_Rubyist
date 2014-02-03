a130c51c0da0e77f56a9671fd2bfc8cd489d573f
Abraham Thomas <dysruptmedia@gmail.com>
2014-02-03 20:25:54 +0000

    Querying ticket object

--- ticket.rb
+++ ticket.rb
@@ -23,3 +23,14 @@ ticket = Object.new
   def ticket.date
     "01/02/03"
   end
+
+  print "This ticket is for: "
+  print ticket.event + ", at "
+  print ticket.venue + ", on "
+  puts ticket.date + "."
+  print "The performer is "
+  puts ticket.performer + "."
+  print "The seat is "
+  print ticket.seat + ", "
+  print "and it costs $"
+  puts "%.2f." % ticket.price
