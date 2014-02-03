b86316a8f8c88386ea1d0278991e1d00518ffdb6
Abraham Thomas <dysruptmedia@gmail.com>
2014-02-03 21:28:57 +0000

    default args

--- args.rb
+++ args.rb
@@ -1,9 +1,6 @@
-obj = Object.new
 
-def two_or_more(a,b,*c)
-  puts "I require two or more arguments!"
-  puts "And sure enough, i got: "
-  p a, b, c
+def default_args(a,b,c=1)
+  puts "Values of variables: ",a,b,c
 end
 
-two_or_more(1,2,3,4,5)
+default_args(3,2)
