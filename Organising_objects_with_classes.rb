40ae46698e20d4a7f135b4bbb847f9d2513fc287
Abraham Thomas <dysruptmedia@gmail.com>
2014-02-04 20:41:35 +0000

    Overriding methods

--- Organising_objects_with_classes.rb
+++ Organising_objects_with_classes.rb
@@ -1,8 +1,11 @@
-class Ticket
-  def event
-    "Can't really be specified yet"
+class C
+  def m
+    puts "First definition of method m"
+  end
+
+  def m
+    puts "Second definition of method m"
   end
 end
 
-ticket = Ticket.new
-puts ticket.event
+C.new.m 
