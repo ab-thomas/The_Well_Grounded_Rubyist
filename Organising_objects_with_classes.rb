4cd41965e8218a785f0a993d082e8f88ac55505e
Abraham Thomas <dysruptmedia@gmail.com>
2014-02-04 21:55:03 +0000

    Equal sign method as gatekeeper or filter

--- Organising_objects_with_classes.rb
+++ Organising_objects_with_classes.rb
@@ -1,19 +1,10 @@
 class Ticket
-  def initialize(venue,date)
-   @venue = venue
-   @date = date
-  end
-
-  def venue
-    @venue
-  end
-
-  def date 
-    @date
-  end
-
-  def price=(amount) # setter method
-    @price = amount
+  def price=(amount)
+    if (amount * 100).to_i == amount *100
+      @price = amount
+    else
+      puts "The price seems to be malformed"
+    end
   end
 
   def price
@@ -21,12 +12,5 @@ class Ticket
   end
 end
 
-ticket = Ticket.new("Town Hall", "11/12/13")
-# ticket.price: 'ticket' is the receiver and 'price' is the method
-# '63.00' is the single argument 
-ticket.price = 63.00 # syntactic sugar
-puts "The ticket cost $#{"%.2f" % ticket.price}."
-ticket.price = 72.50 # syntactic sugar
-puts "Whoops--it just went up. It now costs $#{"%.2f" % ticket.price}."
-
-
+s = Ticket.new
+s.price = 50.35
