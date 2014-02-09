a9d0275a8fd3ab36904e03a86d6858aab0f3c6f5
Abraham Thomas <dysruptmedia@gmail.com>
2014-02-09 19:38:45 +0000

    Modules: Stack examplee

--- Constants.rb
+++ Constants.rb
@@ -2,7 +2,11 @@ class Ticket
   VENUES = ["Convention Center", "Fairgrounds", "Town Hall"]
 end
 
+venues = Ticket::venues
+venues << "High School Gym"
+
 puts "We're closed the class definition."
 puts "So we have to use the path notation to reach the constant."
 puts "The venues are: "
 puts Ticket::VENUES 
+
